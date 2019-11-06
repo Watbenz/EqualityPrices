2058d34df2efd00992f5bc94cf9c940af06012c0
watbenz <watsith@hotmail.com>
2019-11-06 20:21:50 +0700

    colored UI

--- app/src/main/java/com/example/myapplication/CalculatePage.java
+++ app/src/main/java/com/example/myapplication/CalculatePage.java
@@ -2,11 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -18,14 +23,14 @@ public class CalculatePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_page);
-
         init();
     }
 
     private void init() {
         getData();
+        generateMoneyButton();
         generatePerson();
-        generateNextPerson(7);
+        generateNextPerson(8-1);
     }
 
     private void getData() {
@@ -36,13 +41,46 @@ public class CalculatePage extends AppCompatActivity {
         dataCreate = gson.fromJson(json, DataCreate.class);
     }
 
+    @SuppressLint("NewApi")
+    private void generateMoneyButton() {
+        String[] val = {
+                "clear", "1", "5", "10", "20", "50",
+                "100", "500", "1k", "5k", "10k",
+                "20k", "50k", "100k"
+        };
+
+        LinearLayout parent = (LinearLayout) findViewById(R.id.linearLayout_scroll_button);
+        LinearLayout.LayoutParams params = getMoneyButtonParams();
+
+        for (String each : val) {
+            final Button b = new Button(getApplicationContext());
+            b.setText(each);
+            b.setLayoutParams(params);
+            b.setTextColor(getResources().getColor(R.color.white));
+            b.setBackground(getResources().getDrawable(R.drawable.ripple_money_rounded_button));
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    moneyUpdate(b.getText().toString());
+                }
+            });
+            parent.addView(b);
+        }
+    }
+
+    private void moneyUpdate(String s) {
+        Log.i("Debug", "moneyUpdate: " + s);
+    }
+
     private void generatePerson() {
         LinearLayout parent = (LinearLayout) findViewById(R.id.first_person);
         View child = getLayoutInflater().inflate(R.layout.activity_person, null);
         parent.addView(child);
     }
 
-    private void generateNextPerson(int n, boolean flag) {
+    private void generateNextPersonN(int n) {
+        if (n <= 0) return;
+
         LinearLayout.LayoutParams params = getEqualParams();
         LinearLayout horizon = new LinearLayout(getApplicationContext());
         horizon.setOrientation(LinearLayout.HORIZONTAL);
@@ -58,12 +96,25 @@ public class CalculatePage extends AppCompatActivity {
     }
 
     private void generateNextPerson(int n) {
+        if (n <= 0) return;
+
+        setHeadNextPerson();
+
         int row = n/3;
 
         for (int i=0; i<row; i++) {
-            generateNextPerson(3, true);
+            generateNextPersonN(3);
         }
-        generateNextPerson(n%3, true);
+        generateNextPersonN(n%3);
+    }
+
+    private void setHeadNextPerson() {
+        TextView text = new TextView(getApplicationContext());
+        text.setText("รายจ่ายของเพื่อน ๆ");
+        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28f);
+        text.setGravity(Gravity.CENTER);
+        LinearLayout parent = (LinearLayout) findViewById(R.id.next_person_display);
+        parent.addView(text);
     }
 
     private LinearLayout.LayoutParams getEqualParams() {
@@ -73,6 +124,18 @@ public class CalculatePage extends AppCompatActivity {
                 1.0f);
         params.topMargin = 25;
         params.leftMargin = 15;
+
+        return params;
+    }
+
+    private LinearLayout.LayoutParams getMoneyButtonParams() {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.width = 100;
+        params.height = 100;
+        params.rightMargin = 20;
 
         return params;
     }
